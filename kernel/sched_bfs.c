f5270ce7dab064db60297ed70cabf0721629238c
Brandon Berhent <cheater1034@gmail.com>
2009-09-20 01:10:38 -0400

    4) fix bfs

--- kernel/sched_bfs.c
+++ kernel/sched_bfs.c
@@ -5214,6 +5214,7 @@ static struct sched_domain *__build_numa_sched_domains(struct s_data *d,
 	if (cpumask_weight(cpu_map) >
 	    SD_NODES_PER_DOMAIN * cpumask_weight(d->nodemask)) {
 		sd = &per_cpu(allnodes_domains, i).sd;
+		SD_INIT(sd, ALLNODES);
 		set_domain_attribute(sd, attr);
 		cpumask_copy(sched_domain_span(sd), cpu_map);
 		cpu_to_allnodes_group(i, cpu_map, &sd->groups, d->tmpmask);
@@ -5881,22 +5882,27 @@ static inline int preempt_count_equals(int preempt_offset)
 void __might_sleep(char *file, int line, int preempt_offset)
 {
 #ifdef in_atomic
-	static unsigned long prev_jiffy;	/* ratelimiting */
+	static unsigned long prev_jiffy;        /* ratelimiting */
 
-	if ((preempt_count_equals(preempt_offset) && irqs_disabled()) ||
-		system_state != SYSTEM_RUNNING || !oops_in_progress)
-		if (time_before(jiffies, prev_jiffy + HZ) && prev_jiffy)
-			return;
-		prev_jiffy = jiffies;
-		printk(KERN_ERR "BUG: sleeping function called from invalid"
-				" context at %s:%d\n", file, line);
-		printk("in_atomic():%d, irqs_disabled():%d\n",
-			in_atomic(), irqs_disabled());
-		debug_show_held_locks(current);
-		if (irqs_disabled())
-			print_irqtrace_events(current);
-		dump_stack();
-	}
+	if ((preempt_count_equals(preempt_offset) && !irqs_disabled()) ||
+	   system_state != SYSTEM_RUNNING || oops_in_progress)
+		return;
+	if (time_before(jiffies, prev_jiffy + HZ) && prev_jiffy)
+		return;
+	prev_jiffy = jiffies;
+
+	printk(KERN_ERR
+	"BUG: sleeping function called from invalid context at %s:%d\n",
+			file, line);
+	printk(KERN_ERR
+	"in_atomic(): %d, irqs_disabled(): %d, pid: %d, name: %s\n",
+			in_atomic(), irqs_disabled(),
+			current->pid, current->comm);
+
+	debug_show_held_locks(current);
+	if (irqs_disabled())
+		print_irqtrace_events(current);
+	dump_stack();
 #endif
 }
 EXPORT_SYMBOL(__might_sleep);
