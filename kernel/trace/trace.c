6f447716562c8c88ff3df1c660662ce6703c3eab
Brandon Berhent <cheater1034@gmail.com>
2009-10-05 22:43:07 -0400

    trace.c: grunqueue_is_locked needs to be there too

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -275,10 +275,14 @@ unsigned long trace_flags = TRACE_ITER_PRINT_PARENT | TRACE_ITER_PRINTK |
 void trace_wake_up(void)
 {
 	/*
-	 * The runqueue_is_locked() can fail, but this is the best we
+	 * The (g)runqueue_is_locked() can fail, but this is the best we
 	 * have for now:
 	 */
+#ifdef CONFIG_CPU_BFS
+	if (!(trace_flags & TRACE_ITER_BLOCK) && !grunqueue_is_locked())
+#else
 	if (!(trace_flags & TRACE_ITER_BLOCK) && !runqueue_is_locked())
+#endif
 		wake_up(&trace_wait);
 }
 
