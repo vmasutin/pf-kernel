f42c7a6f0effafe54cabd16d10289c379a84742f
Brandon Berhent <brandon@zen-kernel.org>
2009-12-05 01:38:57 -0500

    missing { in sched_bfs.c

--- kernel/sched_bfs.c
+++ kernel/sched_bfs.c
@@ -6469,7 +6469,7 @@ void __might_sleep(char *file, int line, int preempt_offset)
 	static unsigned long prev_jiffy;	/* ratelimiting */
 
 	if ((preempt_count_equals(preempt_offset) && !irqs_disabled()) ||
-	    system_state != SYSTEM_RUNNING || oops_in_progress)
+	    system_state != SYSTEM_RUNNING || oops_in_progress) {
 		if (time_before(jiffies, prev_jiffy + HZ) && prev_jiffy)
 			return;
 		prev_jiffy = jiffies;
@@ -6481,6 +6481,7 @@ void __might_sleep(char *file, int line, int preempt_offset)
 		if (irqs_disabled())
 			print_irqtrace_events(current);
 		dump_stack();
+	}
 #endif
 }
 EXPORT_SYMBOL(__might_sleep);
