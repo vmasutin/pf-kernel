c0ec45911e790989f4a593c163291c985f3b4ac1
Tejun Heo <tj@kernel.org>
2010-06-21 16:04:53 +0200

    irq: implement IRQ expecting
    This patch implements IRQ expecting, which can be used when a driver
    can anticipate the controller to raise an interrupt in relatively
    immediate future.  A driver needs to allocate an irq expect token
    using init_irq_expect() to use it.  expect_irq() should be called when
    an operation which will be followed by an interrupt is started.
    unexpect_irq() when the operation finished or timed out.
    
    This allows IRQ subsystem closely monitor the IRQ and react quickly if
    the expected IRQ doesn't happen for whatever reason.  The
    [un]expect_irq() functions are fairly light weight and any real driver
    which accesses hardware controller should be able to use them for each
    operation without adding noticeable overhead.
    
    This is most useful for drivers which have to deal with hardware which
    is inherently unreliable in dealing with interrupts.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -75,6 +75,8 @@ enum {
 
 typedef irqreturn_t (*irq_handler_t)(int, void *);
 
+struct irq_expect;
+
 struct irq_watch {
 	irqreturn_t		last_ret;
 	unsigned int		flags;
@@ -96,6 +98,7 @@ struct irq_watch {
  * @thread:	thread pointer for threaded interrupts
  * @thread_flags:	flags related to @thread
  * @watch:	data for irq watching
+ * @expects:	data for irq expecting
  */
 struct irqaction {
 	irq_handler_t		handler;
@@ -109,6 +112,7 @@ struct irqaction {
 	struct task_struct	*thread;
 	unsigned long		thread_flags;
 	struct irq_watch	watch;
+	struct irq_expect	*expects;
 };
 
 extern irqreturn_t no_action(int cpl, void *dev_id);
@@ -170,6 +174,9 @@ devm_request_irq(struct device *dev, unsigned int irq, irq_handler_t handler,
 
 extern void devm_free_irq(struct device *dev, unsigned int irq, void *dev_id);
 
+extern struct irq_expect *init_irq_expect(unsigned int irq, void *dev_id);
+extern void expect_irq(struct irq_expect *exp);
+extern void unexpect_irq(struct irq_expect *exp, bool timedout);
 extern void watch_irq(unsigned int irq, void *dev_id);
 
 /*
